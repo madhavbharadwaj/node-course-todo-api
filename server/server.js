15b50caadc290f7cc075ebebd67a78995834f238
A Madhav Bharadwaj <madhavbharadwaj1995@gmail.com>
2018-06-25 22:16:37 +0530

    setup for heroku

--- server/server.js
+++ server/server.js
@@ -1,38 +1,55 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Number
+var app = express();
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
   }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
-// var newTodo = new Todo({
-//   text: 'Cook dinner'
-// });
-//
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
-// }, (e) => {
-//   console.log('Unable to save todo')
-// });
-
-var otherTodo = new Todo({
-  text: 'Feed the cat',
-  completed: true,
-  completedAt: 123
+app.listen(port, () => {
+  console.log(`Started up at port $(port)`);
 });
 
-otherTodo.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
-}, (e) => {
-  console.log('Unable to save', e);
-});
+module.exports = {app};
