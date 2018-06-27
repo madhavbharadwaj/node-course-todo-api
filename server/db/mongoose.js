20947d77bbda1fd4a67e6b734224376ba9d09bbc
A Madhav Bharadwaj <madhavbharadwaj1995@gmail.com>
2018-06-27 11:41:15 +0530

    setup2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+//mongoose.connect("mongodb://madhavcreative:44creative88@ds147390.mlab.com:47390/todoapp");
+mongoose.connect("mongodb://nodetodo:nodetodo1@ds119161.mlab.com:19161/todoapp");
 
 module.exports = {mongoose};
